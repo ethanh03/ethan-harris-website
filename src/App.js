8d370eddb6334ad13d4dc6bd8be2b890e7d9bf1e
Ethan Harris <83778377+ethanh03@users.noreply.github.com>
2026-05-31 15:53:27 -0400

    adding biosynthetica to projects section

--- src/App.js
+++ src/App.js
@@ -6,31 +6,20 @@ import About from './Components/About/About'
 import Projects from './Components/Projects/Projects'
 import Title from './Components/Title/Title'
 import Contact from './Components/Contact/Contact'
-import GamesPage from './pages/GamesPage';
-import Biosynthetica from './pages/Biosynthetica';
+
 
 const App = () => {
   return (
-    <BrowserRouter basename="/ethan-harris-website">
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Background/>
-            <div className="container">
-              <About/>
-              <Title title="Projects" subTitle="What I've Been Up To"/>
-              <Projects/>
-              <Title title="Contact Me" subTitle="Stay in touch"/>
-              <Contact/>
-            </div>
-          </>
-        } />
-      
-        <Route path="/games" element={<GamesPage />} />
-        <Route path="/games/:id" element={<Biosynthetica />} />
-      </Routes>
-    </BrowserRouter>
+      <>
+      <Navbar />
+      <Background />
+      <div className="container">
+        <About />
+        <Title title="Projects" subTitle="What I've Been Up To" />
+        <Projects />
+        <Title title="Contact Me" subTitle="Stay in touch" />
+        <Contact />
+    </div></>
   )
 }
 
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Projects.css'
-import games from '../../assets/games.jpg'
 
 const Projects = () => {
   return (
     <div className="projects">
-        <div className="project">
-            <img src={games} alt=''/>
-            <div className="caption">
-                <p>GAMES</p>
-                <Link to="/games">
-                    <button>View Games</button>
-                </Link>
+        <div className='game-container'>
+            <div className='game-play'>
+                    <iframe src="https://itch.io/embed/4629190" frameborder="0" width="1000" height="167" allowFullScreen><a href="https://ethanh03.itch.io/biosynthetica">Biosynthetica by ethanh03</a></iframe>
+            </div>
+            <div className='game-description'>
+                <p>Biosynthetica is a game my group made for our game development class. I worked on the enemy pathfinding system (A* algorithm), item instantiation system, game cutscenes, and more!</p>
+                <p><strong>Tech Used:</strong> Godot, GDScript</p>
             </div>
         </div>
     </div>
